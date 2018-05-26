36006239545298ecd40905a93b0699cffadb48a8
PeterGill800 <gillp064.319@lgflmail.net>
2018-05-26 18:52:10 +0100

    Added space between gold and xp

--- savedata.js
+++ savedata.js
@@ -3,32 +3,114 @@ var player = {
 	xp: 0,
 	inventory: {
 		helm: [
-			{name: "", image: ""},
-			{name: "helm", image:"url('assets/items/helm.png')"},
-			{name: "helm2", image:"url('assets/items/helm2.png')"}
+			{
+				name: "",
+				image: "",
+				stats: {}
+			},
+			{
+				name: "helm",
+				image:"url('assets/items/helm.png')",
+				stats: {
+					defence: 5
+				}
+			},
+			{
+				name: "helm2",
+				image:"url('assets/items/helm2.png')",
+				stats: {
+					defence: 10
+				}
+			}
 		],
 		chest: [
-			{name: "", image: ""},
-			{name: "chest", image:"url('assets/items/chest.png')"},
-			{name: "chest2", image:"url('assets/items/chest2.png')"}
+			{
+				name: "",
+				image: "",
+				stats: {}
+			},
+			{
+				name: "chest",
+				image:"url('assets/items/chest.png')",
+				stats: {
+					defence: 10
+				}
+			},
+			{
+				name: "chest2",
+				image:"url('assets/items/chest2.png')",
+				stats: {
+					defence: 15
+				}
+			}
 		],
 		greaves: [
-			{name: "", image: ""},
-			{name: "greaves", image:"url('assets/items/greaves.png')"},
-			{name: "greaves2", image:"url('assets/items/greaves2.png')"}
+			{
+				name: "",
+				image: "",
+				stats: {}
+			},
+			{
+				name: "greaves",
+				image:"url('assets/items/greaves.png')",
+				stats: {
+					defence: 8
+				}
+			},
+			{
+				name: "greaves2",
+				image:"url('assets/items/greaves2.png')",
+				stats: {
+					defence: 12
+				}
+			}
 		],
 		boots: [
-			{name: "", image: ""},
-			{name: "boots", image:"url('assets/items/boots.png')"},
-			{name: "boots2", image:"url('assets/items/boots2.png')"}
+			{
+				name: "",
+				image: "",
+				stats: {}
+			},
+			{
+				name: "boots",
+				image:"url('assets/items/boots.png')",
+				stats: {
+					defence: 3
+				}
+			},
+			{
+				name: "boots2",
+				image:"url('assets/items/boots2.png')",
+				stats: {
+					defence: 6
+				}
+			}
 		],
 		weapon: [
-			{name: "", image: ""},
-			{name: "weapon", image:"url('assets/items/weapon.png')"},
-			{name: "weapon2", image:"url('assets/items/weapon2.png')"},
-			{name: "sword", image:"url('assets/items/sword.png')"},
-			{name: "staff", image:"url('assets/items/staff.png')"},
-			{name: "bow", image:"url('assets/items/bow.png')"}
-		],
-	},
+			{
+				name: "",
+				image: ""
+			},
+			{
+				name: "weapon",
+				image:"url('assets/items/weapon.png')"
+			},
+			{
+				name: "weapon2",
+				image:"url('assets/items/weapon2.png')"
+			},
+			{
+				name: "sword",
+				image:"url('assets/items/sword.png')"
+			},
+			{
+				name: "staff",
+				image:"url('assets/items/staff.png')"
+			},
+			{
+				name: "bow",
+				image:"url('assets/items/bow.png')"
+			}
+		]
+	}
 };
