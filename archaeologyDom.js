6d089a767b9c08f628692bc39b3ad771c4fd6db3
PeterGill800 <gillp064.319@lgflmail.net>
2018-06-08 21:42:40 +0100

    Archaeology all works except the filter

--- archaeologyDom.js
+++ archaeologyDom.js
@@ -1,26 +1,54 @@
-document.getElementById("all").style.height = window.innerHeight;
-document.getElementById("all").style.width = window.innerWidth;
-
 var array = [];
 for(var i = 0; i < Object.keys(items).length; i++){
 	for(var x = 0; x < items[Object.keys(items)[i]].length; x++){
 		array.push(items[Object.keys(items)[i]][x]);
 	}
 }
-for(var i = 0; i < array.length; i++){
-	document.getElementById("all").innerHTML += '<div class="box"><img src="'+array[i].image+'" class="img"><p id="name'+i+'" class="para"></p><p id="tier'+i+'" class="para"></p><p id="stats'+i+'" class="para"></p><p id="lore'+i+'" class="para"></p></div>';
-	
-	document.getElementById("name"+i).innerHTML = "<b>"+array[i].name+"</b>";
-	if(array[i].rarity == "common"){
-		document.getElementById("name"+i).style.color = "black";
-	}else if(array[i].rarity == "unique"){
-		document.getElementById("name"+i).style.color = "orange";
+var previousWidth = window.innerWidth;
+checkWidth();
+function checkWidth(){
+	window.requestAnimationFrame(checkWidth);
+	if(window.innerWidth != previousWidth){
+		arrange();
+	}
+	previousWidth = window.innerWidth;
+}
+function radioOnOff(id){
+	if(document.getElementById(id).checked){
+		console.log("no");
+		document.getElementById(id).checked = false;
 	}else{
-		document.getElementById("name"+i).style.color = "purple";
+		console.log("yes");
+		document.getElementById(id).checked = true;
 	}
-	document.getElementById("tier"+i).innerHTML = "<br>Tier: "+array[i].tier;
-	for(var x = 0; x < Object.keys(array[i].stats).length; x++){
-		document.getElementById("stats"+i).innerHTML += Object.keys(array[i].stats)[x]+": "+array[i].stats[Object.keys(array[i].stats)[x]]+"<br>";
+}
+arrange();
+function arrange(){
+	var columns = Math.floor((window.innerWidth-45)/185);
+	document.getElementsByTagName("body")[0].innerHTML = "<div id='searchBar'><input type='radio' id='helm' onclick='radioOnOff(\"helm\")' checked='true'>Helms&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='radio' checked>Chests&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='radio' checked>Greaves&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='radio' checked>Boots&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='radio' checked>Swords&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='radio' checked>Staffs&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='radio' checked>Bows</div>";
+	document.getElementById("searchBar").style.width = (((Math.floor((window.innerWidth-45)/185)))*185)-35+"px";
+	document.getElementById("searchBar").style.left = 25+((window.innerWidth-45)-(((Math.floor((window.innerWidth-45)/185)))*185))/2+"px";
+	for(var i = 0; i < array.length; i++){
+		for(var a = 0; a < columns; a++){
+			if(Math.floor(i/columns)==(i-a)/columns){
+				x=a;
+			}
+		}
+		document.getElementsByTagName("body")[0].innerHTML += '<ul id="flashcardlist'+x+'" class="flashcardlist"></ul>';
+		document.getElementById("flashcardlist"+x).innerHTML += '<li class="box"><img src="'+array[i].image+'" class="img"><p id="name'+i+'" class="para"></p><p id="tier'+i+'" class="para"></p><p id="stats'+i+'" class="para"></p><p id="lore'+i+'" class="para"></p></li>';
+		document.getElementById("flashcardlist"+x).style.left = 25+x*185+((window.innerWidth-45)-(((Math.floor((window.innerWidth-45)/185)))*185))/2+"px";
+		document.getElementById("name"+i).innerHTML = "<b>"+array[i].name+"</b>";
+		if(array[i].rarity == "common"){
+			document.getElementById("name"+i).style.color = "black";
+		}else if(array[i].rarity == "unique"){
+			document.getElementById("name"+i).style.color = "orange";
+		}else{
+			document.getElementById("name"+i).style.color = "purple";
+		}
+		document.getElementById("tier"+i).innerHTML = "<br>Tier: "+array[i].tier;
+		for(var a = 0; a < Object.keys(array[i].stats).length; a++){
+			document.getElementById("stats"+i).innerHTML += Object.keys(array[i].stats)[a]+": "+array[i].stats[Object.keys(array[i].stats)[a]]+"<br>";
+		}
+		document.getElementById("lore"+i).innerHTML = "<br>"+array[i].lore;
 	}
-	document.getElementById("lore"+i).innerHTML = "<br>"+array[i].lore;
 }
