f94020f9d7d9620dcc8f001b16ff851d621daede
PeterGill800 <gillp064.319@lgflmail.net>
2018-06-08 22:03:24 +0100

    Made code more efficient

--- archaeologyDom.js
+++ archaeologyDom.js
@@ -25,7 +25,8 @@ function radioOnOff(id){
 arrange();
 function arrange(){
 	var columns = Math.floor((window.innerWidth-45)/185);
-	document.getElementsByTagName("body")[0].innerHTML = "<div id='searchBar'><input type='radio' id='helm' onclick='radioOnOff(\"helm\")' checked='true'>Helms&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='radio' checked>Chests&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='radio' checked>Greaves&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='radio' checked>Boots&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='radio' checked>Swords&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='radio' checked>Staffs&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='radio' checked>Bows</div>";
+	//document.getElementById("all").innerHTML = "<div id='searchBar'><input type='radio' id='helm' onclick='radioOnOff(\"helm\")' checked='true'>Helms&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='radio' checked>Chests&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='radio' checked>Greaves&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='radio' checked>Boots&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='radio' checked>Swords&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='radio' checked>Staffs&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='radio' checked>Bows</div>";
+	document.getElementById("all").innerHTML = "";
 	document.getElementById("searchBar").style.width = (((Math.floor((window.innerWidth-45)/185)))*185)-35+"px";
 	document.getElementById("searchBar").style.left = 25+((window.innerWidth-45)-(((Math.floor((window.innerWidth-45)/185)))*185))/2+"px";
 	for(var i = 0; i < array.length; i++){
@@ -34,7 +35,7 @@ function arrange(){
 				x=a;
 			}
 		}
-		document.getElementsByTagName("body")[0].innerHTML += '<ul id="flashcardlist'+x+'" class="flashcardlist"></ul>';
+		document.getElementById("all").innerHTML += '<ul id="flashcardlist'+x+'" class="flashcardlist"></ul>';
 		document.getElementById("flashcardlist"+x).innerHTML += '<li class="box"><img src="'+array[i].image+'" class="img"><p id="name'+i+'" class="para"></p><p id="tier'+i+'" class="para"></p><p id="stats'+i+'" class="para"></p><p id="lore'+i+'" class="para"></p></li>';
 		document.getElementById("flashcardlist"+x).style.left = 25+x*185+((window.innerWidth-45)-(((Math.floor((window.innerWidth-45)/185)))*185))/2+"px";
 		document.getElementById("name"+i).innerHTML = "<b>"+array[i].name+"</b>";
