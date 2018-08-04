bd1f4f76a28c4cc1d0245bfc91bc8c9f3d95f628
PeterGill800 <gillp064.319@lgflmail.net>
2018-08-04 20:11:18 +0100

    Improved item builder

--- builder/dom.js
+++ builder/dom.js
@@ -18,7 +18,7 @@ function update(){
 			}
 		}
 	}
-	if(back == ""){
+	if(position == Builder){
 		document.getElementById("back").hidden = true;
 	}else{
 		document.getElementById("back").hidden = false;
@@ -57,58 +57,120 @@ window.addEventListener('mouseup', function(){
 		update();
 	}
 });*/
-
-var back = "";
-var previousWidth = 0;
-var previousHeight = 0;
-function checkChange(){
-window.requestAnimationFrame(checkChange);
-	if(window.innerWidth != previousWidth || window.innerHeight != previousHeight){
-		previousWidth = window.innerWidth;
-		previousHeight = window.innerHeight;
-		update();
-	}
+setList = "<option value=''>None</option>";
+for(var i = 2; i < Object.keys(Items.set).length; i++){
+	setList += "<option value='"+i+"'>"+Items.set[Object.keys(Items.set)[i]].name+"</option>";
 }
-checkChange();
 
 var Builder = {
+	question: "What would you like to build?",
 	item: {
-		helm: {
-			name: "",
-		},
-		chest: {
+		question: "What item would you like to build?",
+		helm: [
+			{
+				question: "Please enter the name:",
+				answer: '<input type="text" id="helmName" placeholder="War Ogre\'s Helm"></input>\
+					<div class="submit" onclick="submit()">Submit</div>',
+				name: "",
+			},
+			{
+				question: "Please enter the tier:",
+				answer: '<input type="number" id="helmTier" min="1" value="1" style="width: 10%; left: 45%;"></input>\
+					<div class="submit" onclick="submit()">Submit</div>',
+				name: "",
+			},
+			{
+				question: "Please select the rarity:",
+				answer: '<select type="select" id="helmRarity">\
+					<option value="common">Common</option>\
+					<option value="unique">Unique</option>\
+					<option value="mythic">Mythic</option>\
+					</select>\
+					<div class="submit" onclick="submit()">Submit</div>',
+				name: "",
+			},
+			{
+				question: "Please enter the defence:",
+				answer: '<input type="number" id="helmDefence" value="0" style="width: 10%; left: 45%;"></input>\
+					<div class="submit" onclick="submit()">Submit</div>',
+				name: "",
+			},
+			{
+				question: "Please select any other stats:",
+				answer: '<input type="checkbox" name="helmStats" value="criticalChance">Critical Chance</input><br>\
+					<input type="checkbox" name="helmStats" value="dodgeChance">Dodge Chance</input><br>\
+					<input type="checkbox" name="helmStats" value="flaming">Flaming</input><br>\
+					<input type="checkbox" name="helmStats" value="focusSpeed">Focus Speed</input><br>\
+					<input type="checkbox" name="helmStats" value="healthRegen">Health Regen</input><br>\
+					<input type="checkbox" name="helmStats" value="looting">Looting</input><br>\
+					<input type="checkbox" name="helmStats" value="poison">Poison</input><br>\
+					<input type="checkbox" name="helmStats" value="reflection">Reflection</input><br>\
+					<input type="checkbox" name="helmStats" value="stun">Stun</input><br>\
+					<input type="checkbox" name="helmStats" value="swimSpeed">Swim Speed</input><br>\
+					<input type="checkbox" name="helmStats" value="walkSpeed">Walk Speed</input><br>\
+					<div class="submit" onclick="submit()">Submit</div>',
+				name: "",
+			},
+			{
+				question: "Please enter the lore if it has one:",
+				answer: '<input type="text" id="helmLore" style="width: 77%; left: 11.5%; font-size: 3vw;" placeholder="Protects you from splinters. And goblins!"></input>\
+					<div class="submit" onclick="submit()">Submit</div>',
+				name: "",
+			},
+			{
+				question: "Please select the set if it has one:",
+				answer: '<select type="select" id="helmSet">\
+					'+setList+'\
+					</select>\
+					<div class="submit" onclick="submit()">Submit</div>',
+				name: "",
+			},
+			/*{
+				question: "What area is this item for?",
+				answer: '<input type="text" id="helmArea" placeholder="Eaglecrest Logging Camp" style="width: 60%; left: 20%;"></input>\
+					<div class="submit" onclick="submit()">Submit</div>',
+				name: "",
+			},*/
+			{
+				question: "How is this item obtained?",
+				answer: '<input type="text" id="helmObtain" style="width: 77%; left: 11.5%; font-size: 3vw;" placeholder="Find as an unidentified item in Eaglecrest Logging Camp"></input>\
+					<div class="submit" onclick="submit()">Submit</div>',
+				name: "",
+			},
+		],
+		chest: [
 			
-		},
-		greaves: {
+		],
+		greaves: [
 			
-		},
-		boots: {
+		],
+		boots: [
 			
-		},
-		sword: {
+		],
+		sword: [
 			
-		},
-		staff: {
+		],
+		staff: [
 			
-		},
-		bow: {
+		],
+		bow: [
 			
-		},
-		set: {
+		],
+		set: [
 			
-		},
-		currency: {
+		],
+		currency: [
 			
-		},
-		bag: {
+		],
+		bag: [
 			
-		},
-		questItem: {
+		],
+		questItem: [
 			
-		},
-		junkItem: {
+		],
+		junkItem: [
 			
-		},
+		],
 	},
 	quest: {
 		
@@ -121,50 +183,94 @@ var Builder = {
 	},
 }
 
+var position = Builder;
+var stage = 0;
+var back0 = undefined;
+var back1 = undefined;
+var previousWidth = 0;
+var previousHeight = 0;
+function checkChange(){
+window.requestAnimationFrame(checkChange);
+	if(window.innerWidth != previousWidth || window.innerHeight != previousHeight){
+		previousWidth = window.innerWidth;
+		previousHeight = window.innerHeight;
+		update();
+	}
+}
+checkChange();
+
 document.getElementById("back").onclick = function(){
-	back();
+	if(back1 != undefined){
+		position = back1;
+		back1 = undefined;
+	}else if(back0 != undefined){
+		position = Builder;
+		back0 = undefined;
+	}
+	document.getElementById("question").innerHTML = position.question;
+	document.getElementById("answer").innerHTML = "";
+	for(var i = 1; i < Object.keys(position).length; i++){
+		var replaceStat = Object.keys(position)[i].replace( /([A-Z])/g, " $1" );
+		document.getElementById("answer").innerHTML += '<div id="'+replaceStat+'" onclick="forward('+i+')">'+replaceStat.charAt(0).toUpperCase() + replaceStat.slice(1)+'</div>';
+	}
+	update();
 }
 
-function index(){
+function forward(num){
+	if(back0 == undefined){
+		back0 = position;
+		position = back0[Object.keys(back0)[num]];
+	}else if(back1 == undefined){
+		back1 = position;
+		position = back1[Object.keys(back1)[num]];
+	}
+	if(position.length != undefined){
+		document.getElementById("question").innerHTML = position[0].question;
+		document.getElementById("answer").innerHTML = position[0].answer;
+	}else{
+		document.getElementById("question").innerHTML = position.question;
+		document.getElementById("answer").innerHTML = "";
+		for(var i = 1; i < Object.keys(position).length; i++){
+			var replaceStat = Object.keys(position)[i].replace( /([A-Z])/g, " $1" );
+			document.getElementById("answer").innerHTML += '<div id="'+replaceStat+'" onclick="forward('+i+')">'+replaceStat.charAt(0).toUpperCase() + replaceStat.slice(1)+'</div>';
+		}
+	}
+	update();
+}
+
+function submit(){
+	stage++;
+	document.getElementById("question").innerHTML = position[stage].question;
+	document.getElementById("answer").innerHTML = position[stage].answer;
+}
+
+/*function index(){
 	document.getElementById("question").innerHTML = 'What would you like to build?';
-	/*document.getElementById("answer").innerHTML = '<div id="item" onclick="item()">Item</div>\
-	<div id="quest">Quest</div>\
-	<div id="npc">NPC</div>\
-	<div id="enemy">Enemy</div>';*/
 	document.getElementById("answer").innerHTML = "";
-	for(var i = 0; i < Object.keys(Builder).length; i++){
+	for(var i = 1; i < Object.keys(Builder).length; i++){
 		var replaceStat = Object.keys(Builder)[i].replace( /([A-Z])/g, " $1" );
 		document.getElementById("answer").innerHTML += '<div id="'+replaceStat+'" onclick="'+replaceStat+'()">'+replaceStat.charAt(0).toUpperCase() + replaceStat.slice(1)+'</div>';
 	}
 	update();
-}
+}*/
 
-function item(){
+/*function item(){
+	back0 = position;
+	position = Builder.item;
 	document.getElementById("question").innerHTML = 'What item would you like to build?';
-	/*document.getElementById("answer").innerHTML = '<div id="helm" onclick="helm()">Helm</div>\
-	<div id="chest">Chest</div>\
-	<div id="greaves">Greaves</div>\
-	<div id="boots">Boots</div>\
-	<div id="sword">Sword</div>\
-	<div id="staff">Staff</div>\
-	<div id="bow">Bow</div>\
-	<div id="set">Set</div>\
-	<div id="currency">Currency</div>\
-	<div id="bag">Bag</div>\
-	<div id="quest item">Quest Item</div>\
-	<div id="junk item">Junk Item</div>';*/
 	document.getElementById("answer").innerHTML = "";
-	for(var i = 0; i < Object.keys(Builder.item).length; i++){
+	for(var i = 1; i < Object.keys(Builder.item).length; i++){
 		var replaceStat = Object.keys(Builder.item)[i].replace( /([A-Z])/g, " $1" );
-		document.getElementById("answer").innerHTML += '<div id="'+replaceStat+'" onclick="'+replaceStat+'()">'+replaceStat.charAt(0).toUpperCase() + replaceStat.slice(1)+'</div>';
+		document.getElementById("answer").innerHTML += '<div id="'+replaceStat+'" onclick="forward('+i+')">'+replaceStat.charAt(0).toUpperCase() + replaceStat.slice(1)+'</div>';
 	}
-	back = index;
 	update();
 }
 
 function helm(){
+	back1 = position;
+	position = Builder.item.helm;
 	document.getElementById("question").innerHTML = 'Please enter a name:';
-	back = item;
 	document.getElementById("answer").innerHTML = '<input type="text" id="helmName"></input>\
 	<div class="submit" onclick="helmTier()">Submit</div>';
-}
+	update();
+}*/
