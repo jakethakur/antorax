cb4d8461f06d7d3d01f3a832651672b84a81d60b
pinkcustard2 <zachthakur@outlook.com>
2022-09-18 20:51:25 +0100

    hopefully actually fixed tag now

--- main.js
+++ main.js
@@ -8351,7 +8351,7 @@ Game.update = function (delta) {
 	}
 
 	// minigames tagged player speedboost
-	if (this.minigameInProgress.taggedPlayer.userID === ws.userID) {
+	if (typeof this.minigameInProgress !== "undefined" && this.minigameInProgress.taggedPlayer.userID === ws.userID) {
 		// player is on
 
 		let existingEffect = this.statusEffects.find(statusEffect => statusEffect.title === "You're On!");
