ae116ee908b07d86337e29ea1ab12c7c2351c369
PandaLegend <46057631+PandaLegend@users.noreply.github.com>
2025-02-15 00:09:52 +0000

    Removed the tsx file in the tilemap folder!

--- gamedata/tilemap/eaglecrest.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.1" name="eaglecrest" tilewidth="60" tileheight="60" tilecount="776" columns="8">
- <image source="../../../Documents/GitHub/antorax/assets/tilemap/eaglecrest.png" width="480" height="5820"/>
-</tileset>
